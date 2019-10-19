393bf6c59d55229a0dac7904951ef5a5473e6b40
Alireza6677 <alireza6677@gmail.com>
2019-10-19 19:49:52 +0330

    Fix Readme.md

--- Readme.md
+++ Readme.md
@@ -2,15 +2,17 @@
 
 BingeWatch is a Linux Gtk app that helps you organize and watch your movies and tv shows.
 
-![bingewatch_screenshot](screenshot.png)
+<p align="center">
+  <img alt="bingewatch_screenshot" src="screenshot.png">
+</p>
 
-# :hammer_and_wrench: Build
+# 🛠 Build
 
 You need `ldc` and `dub` installed on your system.
 
 First get your themoviedb.org API_KEY from [here](https://www.themoviedb.org/settings/api)
 
-Then Add it to [source/config.d:7](source/config.d#L7):
+Then Add it to [source/config.d:5](source/config.d#L5):
 
 ```D
 static {
--- source/config.d
+++ source/config.d
@@ -1,8 +1,17 @@
 module config;
 
-import std.process;
-
 static {
     // REPLACE WITH YOUR OWN API KEY
     immutable API_KEY = "";
 }
+
+static immutable string[] supportedMimeTypes = [
+    "video/3gpp", "video/dv", "video/fli", "video/flv", "video/mp4",
+    "video/mp4v-es", "video/mpeg", "video/msvideo", "video/ogg",
+    "video/quicktime", "video/vivo", "video/vnd.divx", "video/vnd.rn-realvideo",
+    "video/vnd.vivo", "video/x-anim", "video/x-avi", "video/x-flc", "video/x-fli",
+    "video/x-flic", "video/x-flv", "video/x-m4v", "video/x-matroska",
+    "video/x-mpeg", "video/x-ms-asf", "video/x-msvideo", "video/x-ms-wm",
+    "video/x-ms-wmv", "video/x-nsv", "video/x-ogm+ogg", "video/x-theora+ogg",
+    "x-content/video-dvd", "x-content/video-vcd", "x-content/video-svcd"
+];
